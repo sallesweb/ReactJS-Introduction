c49f8704278e5ce406bccaf003bcf763baaa3eb6
Felipe Salles <felipesalles.ti@gmail.com>
2022-07-02 07:08:05 -0300

    Props - working with method through props

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     <div className="App">
       <h1>Testando Eventos</h1>
       <Evento numero="1" />
-      <Evento numero="2" />
       <Form />
       <Frase />
       <Frase />
