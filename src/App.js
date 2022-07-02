971d8e76f95ff45a7af45f76149e525bf490588d
Felipe Salles <felipesalles.ti@gmail.com>
2022-07-01 22:49:50 -0300

    Including global and component styles with CSS

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Priscilla Salles";
 
   return (
     <div className="App">
+      <Frase />
+      <Frase />
       <HelloWorld />
       <SayMyName nome="Felipe Salles" />
       <SayMyName nome={nome} />
