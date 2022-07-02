f5fe8b2c3c850e34f423e7bb606be3cf472ee7b5
Felipe Salles <felipesalles.ti@gmail.com>
2022-07-01 22:33:28 -0300

    Props usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa';
 
 function App() {
   const nome = "Priscilla Salles";
@@ -10,6 +11,12 @@ function App() {
       <HelloWorld />
       <SayMyName nome="Felipe Salles" />
       <SayMyName nome={nome} />
+      <Pessoa
+        nome="Felipe Salles"
+        idade="32"
+        profissao="Engenheiro de Software"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
